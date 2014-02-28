8ad34849cef8efb494a85c0358f9dd5f1ce55745
Federico Boschetti <federico.boschetti@yahoo.com>
2014-02-28 10:04:04 +0100

    test on InitBean

--- src/main/java/eu/himeros/cophi/ocr/proofreader/controller/bean/InitBean.java
+++ src/main/java/eu/himeros/cophi/ocr/proofreader/controller/bean/InitBean.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * last update 20140227171400
+ * last update 20140228100000
  *
 */
 
